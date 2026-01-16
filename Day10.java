e2d8757d768ebf22a3f5685889b4e57f873fd89a
Hirva <hirvathaker2006@gmail.com>
2026-01-16 23:01:07 +0530

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,20 @@
+//Ceil in a sorted array
+
+class Solution {
+    public int findCeil(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] >= x) {
+                ans = mid;     
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
