61e1220cef11558da33be7cc5dfad3186b44efbd
Hirva <hirvathaker2006@gmail.com>
2026-01-21 20:16:16 +0530

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,22 @@
+//Delete node in LL without head pointer
+
+/*
+class Node
+{
+    int data ;
+    Node next;
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public void deleteNode(Node del_node) {
+        del_node.data = del_node.next.data;
+        
+        del_node.next = del_node.next.next;
+    }
+}
