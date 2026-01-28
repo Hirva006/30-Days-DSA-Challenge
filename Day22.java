915816f2e95f5c14369c262943817f1946a11aae
Hirva <hirvathaker2006@gmail.com>
2026-01-28 22:40:48 +0530

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,17 @@
+//Search array with adjacent diff atmost k
+// User function Template for Java
+
+class Solution {
+    public int findStepKeyIndex(int[] arr, int k, int x) {
+        // code here
+        int i=0;
+        while(i < arr.length){
+            if(arr[i] == x){
+                return i;
+            }
+            int jump = Math.max(1, Math.abs(arr[i] - x) / k);
+            i += jump;
+        }
+        return -1;
+    }
+}
