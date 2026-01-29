b3eebe74c309a358cc660887dba03c4e37e51ce2
Hirva <hirvathaker2006@gmail.com>
2026-01-29 19:28:20 +0530

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,32 @@
+//Search a node in bst
+
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public boolean search(Node root, int key) {
+        // code here
+        if(root == null) return false;
+        
+        while(root != null){
+            if(root.data == key){
+                return true;
+            }else if(root.data > key){
+                root = root.left;
+            }else{
+                root = root.right;
+            }
+        }
+        return false;
+    }
+}
